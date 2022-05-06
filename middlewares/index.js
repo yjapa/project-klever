5d93bd744e029808ef1af9eefef6bce8888db329
Japa <wesleyy_@live.com>
2022-05-06 17:17:01 -0300

    feat: add address validation in index

---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+const domainError = require('./domainError');
+const error = require('./error');
+const addressValidation = require('./addressValidation');
+
+module.exports = {
+  domainError,
+  error,
+  addressValidation,
+};
