cb4a17b83eaa5101c5fab5346f74d5cd7b0af594
Japa <wesleyy_@live.com>
2022-05-06 17:17:20 -0300

    feat: add domainError

---
+++ middlewares/domainError.js
@@ -0,0 +1,14 @@
+module.exports = (err, _req, res, next) => {
+  const errorMap = {
+    invalidFields: 401,
+    notFound: 404,
+  };
+
+  const status = errorMap[err.code];
+
+  if (!status) {
+    return next(err);
+  }
+
+  return res.status(status).json({ message: err.message });
+};
