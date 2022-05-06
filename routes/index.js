e70c4391eea48b5a2ba4afe51973431460a513f5
Japa <wesleyy_@live.com>
2022-05-06 12:49:54 -0300

    feat: add index routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const DetailsRoute = require('./DetailsRoute');
+const BalanceRoute = require('./BalanceRoute');
+const sendTransactionRoute = require('./sendTransactionRoute');
+const receiveTransactionRoute = require('./receiveTransactionRoute');
+
+module.exports = {
+  DetailsRoute,
+  BalanceRoute,
+  sendTransactionRoute,
+  receiveTransactionRoute,
+};
