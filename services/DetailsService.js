8b045cbfdbaf7814f25d18706f3bab46a6a87957
Japa <wesleyy_@live.com>
2022-05-06 17:15:31 -0300

    feat: validation not found details

--- services/DetailsService.js
+++ services/DetailsService.js
@@ -2,22 +2,31 @@ const DetailsModel = require('../models/DetailsModel');
 const { balanceAddress } = require('../services/BalanceService');
 
 const detailsAddress = async (address) => {
-  const data = await DetailsModel.detailsAddress(address);
-
-  const balance = await balanceAddress(address);
-
-  const details = {
-    address: data.address,
-    balance: data.balance,
-    totalTx: data.txs,
-    balances: { ...balance },
-    total: {
-      sent: data.totalSent,
-      received: data.totalReceived,
-    },
-  };
-
-  return details;
+  try { 
+    const { data } = await DetailsModel.detailsAddress(address);
+  
+    const balance = await balanceAddress(address);
+  
+    const details = {
+      address: data.address,
+      balance: data.balance,
+      totalTx: data.txs,
+      balances: { ...balance },
+      total: {
+        sent: data.totalSent,
+        received: data.totalReceived,
+      },
+    };
+  
+    return details;
+    
+  } catch (err) {
+    const error = {
+      code: 'notFound',
+      message: 'Address not found',
+    };
+    throw error;
+  }
 };
 
 module.exports = {
