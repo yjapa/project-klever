092145f1efb0476acc998395ff436501330d1f67
Japa <wesleyy_@live.com>
2022-05-06 17:18:02 -0300

    feat: add middlewares in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const { connection } = require('./connection');
 const { DetailsRoute, BalanceRoute, sendTransactionRoute, receiveTransactionRoute } = require('./routes')
+const middlewares = require('./middlewares');
 
 app.use(express.json());
 
@@ -10,11 +11,15 @@ const startServer = () => {
 }
 
 startServer();
-// connection();
+
 app.use('/details', DetailsRoute);
 app.use('/balance', BalanceRoute);
 app.use('/balance', BalanceRoute);
 app.use('/send', sendTransactionRoute);
 app.use('/tx', receiveTransactionRoute);
+
+app.use(middlewares.domainError);
+app.use(middlewares.error);
+
 
 app.listen(3000);
