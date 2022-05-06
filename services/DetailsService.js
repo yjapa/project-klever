ab979eacf9aede8f917e1f3d087dab5637b8e4e7
Japa <wesleyy_@live.com>
2022-05-06 15:13:43 -0300

    fix: modify return details in service

--- services/DetailsService.js
+++ services/DetailsService.js
@@ -1,7 +1,21 @@
 const DetailsModel = require('../models/DetailsModel');
+const { balanceAddress } = require('../services/BalanceService');
 
 const detailsAddress = async (address) => {
-  const details = await DetailsModel.detailsAddress(address);
+  const data = await DetailsModel.detailsAddress(address);
+
+  const balance = await balanceAddress(address);
+
+  const details = {
+    address: data.address,
+    balance: data.balance,
+    totalTx: data.txs,
+    balances: { ...balance },
+    total: {
+      sent: data.totalSent,
+      received: data.totalReceived,
+    },
+  };
 
   return details;
 };
