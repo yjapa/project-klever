817b6c9c7f5bee3810889046be8755a889dd1b11
Japa <wesleyy_@live.com>
2022-05-06 12:42:23 -0300

    feat: create connection mongoose/mongodb

---
+++ connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const connection = (mongoDatabaseURI = 'mongodb://localhost:27017/klever_challenger') =>
+  mongoose.connect(mongoDatabaseURI);
+
+module.exports = {
+  connection
+};
