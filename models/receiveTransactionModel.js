c6b74ccbca00faae92855680645aee9a4c65d51e
Japa <wesleyy_@live.com>
2022-05-06 12:46:47 -0300

    feat: add receiveTransactionModel

---
+++ models/receiveTransactionModel.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const https = require('https');
+
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
+
+const receiveTransaction = async (transaction) => {
+  const { data } = await axios
+  .get(
+    `https://blockbook-bitcoin.tronwallet.me/api/v2/tx/${transaction}`,
+    { httpsAgent: agent }
+  );
+
+  return data;
+}
+
+module.exports = {
+  receiveTransaction,
+};
