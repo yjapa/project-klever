1b8405d192b66d0aee7d23c3d99073038023a0df
Japa <wesleyy_@live.com>
2022-05-06 15:07:17 -0300

    feat: add function sumBalance

--- services/BalanceService.js
+++ services/BalanceService.js
@@ -1,9 +1,25 @@
 const BalanceModel = require('../models/BalanceModel');
 
+const sumBalance = (data) => data.reduce((acc, curr) => {
+  if (curr.confirmations < 2) {
+    acc.unconfirmed += Number(curr.value);
+
+    return acc;
+  }
+  acc.confirmed += Number(curr.value);
+
+  return acc
+}, {
+  confirmed: 0,
+  unconfirmed: 0,
+});
+
 const balanceAddress = async (address) => {
   const data = await BalanceModel.balanceAddress(address);
 
-  return data;
+  const balance = sumBalance(data);
+
+  return balance;
 };
 
 module.exports = {
