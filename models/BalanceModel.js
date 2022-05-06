ceff5d79932aef8e0a47e2aeefef270309bdc5c5
Japa <wesleyy_@live.com>
2022-05-06 12:46:30 -0300

    feat: add BalanceModel

---
+++ models/BalanceModel.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+const https = require('https');
+
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
+
+const balanceAddress = async (address) => {
+  const { data } = await axios
+  .get(
+    `https://blockbook-bitcoin.tronwallet.me/api/v2/utxo/${address}`,
+    { httpsAgent: agent }
+  );
+
+  return data;
+}
+
+module.exports = {
+  balanceAddress,
+};
