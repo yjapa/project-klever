adab3c46dd47e4cfd25fe1d0153390908456bd0a
Japa <wesleyy_@live.com>
2022-05-06 12:40:23 -0300

    feat: add DetailsController

---
+++ controllers/DetailsController.js
@@ -0,0 +1,12 @@
+const DetailsService = require('../services/DetailsService');
+
+const detailsAddress = async (req, res) => {
+  const { address } = req.params;
+  const details = await DetailsService.detailsAddress(address);
+
+  return res.status(200).json(details);
+};
+
+module.exports = {
+  detailsAddress,
+};
