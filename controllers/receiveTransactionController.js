c877d84d13a5b50a9f67d1bb49c4f1e94e3bb6ed
Japa <wesleyy_@live.com>
2022-05-06 12:46:04 -0300

    feat: add receiveTransactionController

---
+++ controllers/receiveTransactionController.js
@@ -0,0 +1,12 @@
+const receiveTransactionService = require('../services/receiveTransactionService');
+
+const receiveTransaction = async (req, res) => {
+  const { transaction } = req.params;
+  const receive = await receiveTransactionService.receiveTransaction(transaction);
+
+  return res.status(200).json(receive);
+};
+
+module.exports = {
+  receiveTransaction,
+};
