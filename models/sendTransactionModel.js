196f38c0e414a64d650b5dcbdbc24180cda967e1
Japa <wesleyy_@live.com>
2022-05-06 15:14:08 -0300

    fix: modify return sendTransaction in model

--- models/sendTransactionModel.js
+++ models/sendTransactionModel.js
@@ -3,17 +3,20 @@ const TransactionSchema = require('../schemas/TransactionSchema');
 
 const transactionModel = model('transactions', TransactionSchema);
 
-const createTransaction = async (obj) => {
-  const create = await transactionModel.create(obj);
-
-  return create
-};
-
 const getTransactions = async () => {
   const transactions = await transactionModel.find({}, { _id: 0 });
 
   return { utxos: transactions };
 };
+
+const createTransaction = async (obj) => {
+  await transactionModel.create(obj);
+
+  const transactions = getTransactions();
+
+  return transactions;
+};
+
 
 module.exports = {
   getTransactions,
