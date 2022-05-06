d4bac3217e6e768073b5e1f9a834fafdecd5f178
Japa <wesleyy_@live.com>
2022-05-06 17:15:55 -0300

    feat: add validation address in route

--- routes/DetailsRoute.js
+++ routes/DetailsRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const { detailsAddress } = require('../controllers/DetailsController');
+const { addressValidation } = require('../middlewares');
 
 const details = express.Router();
 
-details.get('/:address', rescue(detailsAddress));
+details.get('/:address', addressValidation, rescue(detailsAddress));
 
 module.exports = details;
