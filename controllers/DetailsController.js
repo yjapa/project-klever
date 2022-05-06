916ba8dd945ae0b4ad8f570ed8f4a29792567d02
Japa <wesleyy_@live.com>
2022-05-06 17:21:09 -0300

    fix: remove conditional

--- controllers/DetailsController.js
+++ controllers/DetailsController.js
@@ -2,6 +2,7 @@ const DetailsService = require('../services/DetailsService');
 
 const detailsAddress = async (req, res) => {
   const { address } = req.params;
+
   const details = await DetailsService.detailsAddress(address);
 
   return res.status(200).json(details);
