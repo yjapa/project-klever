483e991f5e7ff7decfad240c6960ff723dd9c5d3
Japa <wesleyy_@live.com>
2022-05-10 20:17:55 -0300

    feat: add transactionValidation in route

--- routes/receiveTransactionRoute.js
+++ routes/receiveTransactionRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const { receiveTransaction } = require('../controllers/receiveTransactionController');
+const { transactionValidation } = require('../middlewares');
 
 const transaction = express.Router();
 
-transaction.get('/:transaction', rescue(receiveTransaction));
+transaction.get('/:transaction', transactionValidation, rescue(receiveTransaction));
 
 module.exports = transaction;
