63c4c6d28f9991aad4a394493c30961c5ca9bdc6
Japa <wesleyy_@live.com>
2022-05-10 20:17:40 -0300

    feat: add healtCheck in index routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const DetailsRoute = require('./DetailsRoute');
 const BalanceRoute = require('./BalanceRoute');
 const sendTransactionRoute = require('./sendTransactionRoute');
 const receiveTransactionRoute = require('./receiveTransactionRoute');
+const HealthCheck = require('./HealthCheckRoute');
 
 module.exports = {
   DetailsRoute,
   BalanceRoute,
   sendTransactionRoute,
   receiveTransactionRoute,
+  HealthCheck,
 };
