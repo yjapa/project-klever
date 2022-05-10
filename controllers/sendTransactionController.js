e419a8b0ab846dc0829130cd175de72fdce6cebd
Japa <wesleyy_@live.com>
2022-05-10 20:15:28 -0300

    fix: remove getTransaction Controller

--- controllers/sendTransactionController.js
+++ controllers/sendTransactionController.js
@@ -1,19 +1,13 @@
 const sendTransactionService = require('../services/sendTransactionService');
+const StatusCode = require('../utils/StatusCode');
 
 const createTransaction = async (req, res) => {
-  const data = req.body
+  const data = req.body;
   const create = await sendTransactionService.createTransaction(data);
 
-  return res.status(200).json(create);
-};
-
-const getTransactions = async (_req, res) => {
-  const transactions = await sendTransactionService.getTransactions();
-
-  return res.status(200).json(transactions);
+  return res.status(StatusCode.CREATED).json(create);
 };
 
 module.exports = {
   createTransaction,
-  getTransactions,
 };
