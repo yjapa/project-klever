996b54ef33417246419e3761e6159247177956cb
Japa <wesleyy_@live.com>
2022-05-10 20:19:44 -0300

    fix: modify error joi

--- validation/addressJoi.js
+++ validation/addressJoi.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const addressJoi = Joi.object({
   address: Joi.string().required().length(42).messages({
     'string.base': '"address" must be a string',
-    'string.length': 'Invalid address',
+    'string.length': 'Invalid address, decoded address is of unknown format',
   }),
 });
 
