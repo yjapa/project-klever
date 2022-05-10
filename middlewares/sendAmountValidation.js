a0ad02d0b9b2b919c3bf61ec1252204d736a475b
Japa <wesleyy_@live.com>
2022-05-10 20:16:10 -0300

    feat: add sendAmountJoi

---
+++ middlewares/sendAmountValidation.js
@@ -0,0 +1,13 @@
+const { sendAmountJoi } = require('../validation/sendAmountJoi');
+
+module.exports = (req, res, next) => {
+  const data = req.body;
+
+  const { error } = sendAmountJoi.validate(data);
+
+  if (error) {
+    return res.status(401).json({ message: error.message });
+  }
+  
+  next();
+};
