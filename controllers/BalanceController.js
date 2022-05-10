5567a082af93008a6d8169aa4c1c66c600e1aa0f
Japa <wesleyy_@live.com>
2022-05-10 20:13:51 -0300

    chore: add StatusCode

--- controllers/BalanceController.js
+++ controllers/BalanceController.js
@@ -1,10 +1,11 @@
 const BalanceService = require('../services/BalanceService');
+const StatusCode = require('../utils/StatusCode');
 
 const balanceAddress = async (req, res) => {
   const { address } = req.params;
   const data = await BalanceService.balanceAddress(address);
 
-  return res.status(200).json(data);
+  return res.status(StatusCode.OK).json(data);
 };
 
 module.exports = {
