c7aa3492079e5fa518e697eb279b68cbf35d6e5b
Japa <wesleyy_@live.com>
2022-05-10 20:16:25 -0300

    feat: add transactionValidation

---
+++ middlewares/transactionValidation.js
@@ -0,0 +1,13 @@
+const { transactionJoi } = require('../validation/transactionJoi');
+
+module.exports = (req, res, next) => {
+  const transaction = req.params;
+
+  const { error } = transactionJoi.validate(transaction);
+
+  if (error) {
+    return res.status(401).json({ message: error.message });
+  }
+  
+  next();
+};
