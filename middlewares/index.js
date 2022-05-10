93a601f2458c875a7f9331beb99e337a3c72e84d
Japa <wesleyy_@live.com>
2022-05-10 20:15:48 -0300

    feat: add and rename in index middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,13 @@
 const domainError = require('./domainError');
 const error = require('./error');
 const addressValidation = require('./addressValidation');
+const transactionValidation = require('./transactionValidation');
+const sendAmountValidation = require('./sendAmountValidation');
 
 module.exports = {
   domainError,
   error,
   addressValidation,
+  transactionValidation,
+  sendAmountValidation,
 };
