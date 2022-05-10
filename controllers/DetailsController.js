bc322620513ebd30f9549bccce5eeb6868404582
Japa <wesleyy_@live.com>
2022-05-10 20:14:10 -0300

    chore: add StatusCode DetailsController

--- controllers/DetailsController.js
+++ controllers/DetailsController.js
@@ -1,11 +1,12 @@
 const DetailsService = require('../services/DetailsService');
+const StatusCode = require('../utils/StatusCode');
 
 const detailsAddress = async (req, res) => {
   const { address } = req.params;
 
   const details = await DetailsService.detailsAddress(address);
 
-  return res.status(200).json(details);
+  return res.status(StatusCode.OK).json(details);
 };
 
 module.exports = {
