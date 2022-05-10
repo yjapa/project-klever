95fa9622d9047ff0ce15f339967a7d8ff2160a43
Japa <wesleyy_@live.com>
2022-05-10 20:14:21 -0300

    fix: lint

--- controllers/receiveTransactionController.js
+++ controllers/receiveTransactionController.js
@@ -2,6 +2,7 @@ const receiveTransactionService = require('../services/receiveTransactionService
 
 const receiveTransaction = async (req, res) => {
   const { transaction } = req.params;
+
   const receive = await receiveTransactionService.receiveTransaction(transaction);
 
   return res.status(200).json(receive);
