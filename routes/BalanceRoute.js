484ad2290e965fd44630c7b88ee4629c259f000e
Japa <wesleyy_@live.com>
2022-05-10 20:17:11 -0300

    feat: add address validation in balanceRoute

--- routes/BalanceRoute.js
+++ routes/BalanceRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const rescue = require('express-rescue');
 const { balanceAddress } = require('../controllers/BalanceController');
+const { addressValidation } = require('../middlewares');
 
 const balance = express.Router();
 
-balance.get('/:address', rescue(balanceAddress));
+balance.get('/:address', addressValidation, rescue(balanceAddress));
 
 module.exports = balance;
