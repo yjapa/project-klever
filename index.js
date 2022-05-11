59a1e766dc04bb6381155cfdbebb064860603d83
Japa <wesleyy_@live.com>
2022-05-11 20:59:51 -0300

    fix: modify index.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-const express = require('express');
-const app = express();
-const { connection } = require('./connection');
-const middlewares = require('./middlewares');
-const { DetailsRoute, BalanceRoute, sendTransactionRoute, receiveTransactionRoute, HealthCheck } = require('./routes')
+const app = require('./server');
 
-app.use(express.json());
-
-const startServer = () => {
-  connection();
-}
-
-startServer();
-
-app.use('/health', HealthCheck);
-app.use('/details', DetailsRoute);
-app.use('/balance', BalanceRoute);
-app.use('/send', sendTransactionRoute);
-app.use('/tx', receiveTransactionRoute);
-
-app.use(middlewares.domainError);
-app.use(middlewares.error);
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('rodando na porta 3000');
+});
