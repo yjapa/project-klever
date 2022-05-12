6827f199b8fe04aa0b7332f310b5b435b742b0ef
Japa <wesleyy_@live.com>
2022-05-12 17:57:04 -0300

    fix: lint

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,7 @@
-module.exports = (_err, _req, res, _next) => {
-   res.status(500).json({
-    code: 'internal_server_error', message: 'error processing request',
-  });
+module.exports = (_err, _req, res, next) => {
+	res.status(500).json({
+		code: 'internal_server_error', message: 'error processing request',
+	});
+
+	next();
 };
