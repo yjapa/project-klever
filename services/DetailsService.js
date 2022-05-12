0ea268cf5d6edeb1bd94e439c9f7dc534aeda820
Japa <wesleyy_@live.com>
2022-05-12 17:57:57 -0300

    fix: modify the function return

--- services/DetailsService.js
+++ services/DetailsService.js
@@ -1,34 +1,34 @@
 const DetailsModel = require('../models/DetailsModel');
-const { balanceAddress } = require('../services/BalanceService');
 
 const detailsAddress = async (address) => {
-  try { 
-    const { data } = await DetailsModel.detailsAddress(address);
-  
-    const balance = await balanceAddress(address);
-  
-    const details = {
-      address: data.address,
-      balance: data.balance,
-      totalTx: data.txs,
-      balances: { ...balance },
-      total: {
-        sent: data.totalSent,
-        received: data.totalReceived,
-      },
-    };
-  
-    return details;
+	try {
+		const { data } = await DetailsModel.detailsAddress(address);
+
+		const details = {
+			address: data.address,
+			balance: data.balance,
+			totalTx: data.txs,
+			balances: {
+				confirmed: data.unconfirmedBalance.toString(),
+				unconfirmed: data.unconfirmedTxs.toString(),
+			},
+			total: {
+				sent: data.totalSent,
+				received: data.totalReceived,
+			},
+		};
+
+		return details;
     
-  } catch (err) {
-    const error = {
-      code: 'notFound',
-      message: 'Address not found',
-    };
-    throw error;
-  }
+	} catch (err) {
+		const error = {
+			code: 'notFound',
+			message: 'Address not found',
+		};
+		throw error;
+	}
 };
 
 module.exports = {
-  detailsAddress,
+	detailsAddress,
 };
