40fd0f2eec23a10f95ff2004b06d0aa69d624850
Japa <wesleyy_@live.com>
2022-05-11 21:00:12 -0300

    feat: add server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const { connection } = require('./connection');
+const middlewares = require('./middlewares');
+const { DetailsRoute, BalanceRoute, sendTransactionRoute, receiveTransactionRoute, HealthCheck } = require('./routes')
+
+app.use(express.json());
+
+const startServer = () => {
+  connection();
+}
+
+startServer();
+
+app.use('/health', HealthCheck);
+app.use('/details', DetailsRoute);
+app.use('/balance', BalanceRoute);
+app.use('/send', sendTransactionRoute);
+app.use('/tx', receiveTransactionRoute);
+
+app.use(middlewares.domainError);
+app.use(middlewares.error);
+
+module.exports = app;
