22a8c03f6b69ebcb679c53a73485bd238eee0528
Japa <wesleyy_@live.com>
2022-05-12 18:57:00 -0300

    feat: add getSummary

---
+++ utils/Summary.js
@@ -0,0 +1,29 @@
+const getSummary = async () => {
+
+	const summary = {
+		Summary: [
+			{
+				description: 'Retorna saldos e transações de um endereço de Bitcoin',
+				route: '/details/{address}',
+			},
+			{
+				description: 'Retorna uma lista de transações confirmadas e não confirmadas',
+				route: '/balance/{address}',
+			},
+			{
+				description: 'Enviar transações de bct',
+				route: '/send',
+			},
+			{
+				description: 'Retornar dados "normalizados" sobre a transação',
+				route: '/tx/{transaction}',
+			}
+		]
+	};
+
+	return summary;
+};
+
+module.exports = {
+	getSummary,
+};
