8df351ed4f386dc285fc140058e831f23132f58c
Japa <wesleyy_@live.com>
2022-05-12 17:56:51 -0300

    fix: domain error

--- middlewares/domainError.js
+++ middlewares/domainError.js
@@ -1,14 +1,14 @@
 module.exports = (err, _req, res, next) => {
-  const errorMap = {
-    invalidFields: 401,
-    notFound: 404,
-  };
+	const errorMap = {
+		invalidFields: 401,
+		notFound: 404,
+	};
 
-  const status = errorMap[err.code];
+	const status = errorMap[err.code];
 
-  if (!status) {
-    return next(err);
-  }
+	if (!status) {
+		return next(err);
+	}
 
-  return res.status(status).json({ message: err.message });
+	return res.status(status).json({ message: err.message });
 };
