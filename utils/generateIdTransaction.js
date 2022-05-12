4098ec00fbdbf3a615c14eb3a9a16fe08e62a3d8
Japa <wesleyy_@live.com>
2022-05-12 18:00:43 -0300

    refactor: generate ID transaction

--- utils/generateIdTransaction.js
+++ utils/generateIdTransaction.js
@@ -1,12 +1,12 @@
 const  generateIdTransaction = () => {
-  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result1= '';
-  const charactersLength = characters.length;
-  for ( let i = 0; i < 64; i++ ) {
-      result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
+	const characters ='abcdefghijklmnopqrstuvwxyz0123456789';
+	let result1= '';
+	const charactersLength = characters.length;
+	for ( let i = 0; i < 64; i++ ) {
+		result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
 
-  return result1;
-}
+	return result1;
+};
 
 module.exports = generateIdTransaction;
