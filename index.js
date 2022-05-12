3b20948fdcd86e6b083cafed47fe5d626971ccf4
Japa <wesleyy_@live.com>
2022-05-12 17:53:49 -0300

    fix: lint index

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require('./server');
-require('dotenv/config')
+require('dotenv/config');
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`API rodando na porta ${PORT}`);
+	console.log(`API rodando na porta ${PORT}`);
 });
