32e1f955dd45fee94b1f8938c0acaac93abb0a1d
Japa <wesleyy_@live.com>
2022-05-11 23:26:48 -0300

    feat: add dotenv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app = require('./server');
+require('dotenv/config')
 
-app.listen(3000, () => {
-  console.log('rodando na porta 3000');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`API rodando na porta ${PORT}`);
 });
